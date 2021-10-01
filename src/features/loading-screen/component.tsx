b7a597ac79653a9f5db81dee2b75efa74b1bafb3
Shikhar Sharma <ceoshikhar@gmail.com>
2021-10-02 01:24:32 +0530

    feat: create loading screen

---
+++ src/features/loading-screen/component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { LoadingSpinner } from '@/components/loading-spinner';
+
+export const LoadingScreen = () => {
+    return (
+        <div>
+            <LoadingSpinner size={24} />
+        </div>
+    );
+};
